fe54308ba1a9dcef341aa9a92cfe507238e68b66
Snezhana <snowwe25@rambler.ru>
2018-09-15 13:17:49 +0300

    version_1

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let loveTriangles = 0;
+    for (let i = 0; i < preferences.length; i++) {
+        if (preferences[i] !== i + 1) {
+            let theFirstSpichonee = preferences[i];
+            let theSecondSpichonee = preferences[theFirstSpichonee - 1];
+            let theThirdSpichonee = preferences[theSecondSpichonee - 1];
+            if (i === theThirdSpichonee - 1) {
+                loveTriangles++;
+                preferences[i] = 0;
+                preferences[(theFirstSpichonee - 1)] = 0;
+                preferences[(theSecondSpichonee - 1)] = 0;
+            }
+        }
+    }
+    return loveTriangles;
 };
